15e68acbbc4b2167f2b2752e47b8185a32dc7b1b
v4kst1z <dcydane@gmail.com>
2021-09-12 20:34:58 +0800

    update

--- test/heapmap.cpp
+++ test/heapmap.cpp
@@ -7,47 +7,49 @@
 class LinkedNode final : public GarbageCollected<LinkedNode> {
  public:
   LinkedNode(LinkedNode *next, int value) : next(next), value_(value) {}
-  void Trace(Visitor *visitor) const override {
-    visitor->Trace(next);
-  }
+  void Trace(Visitor *visitor) const override { visitor->Trace(next); }
+
  private:
   Member<LinkedNode> next;
   int value_;
 };
 
 void test1() {
-  HeapMap<int, std::string> *p = MakeGarbageCollected < HeapMap < int, std::string>>();
+  HeapMap<int, std::string> *p =
+      MakeGarbageCollected<HeapMap<int, std::string>>();
   p->insert(1, "aaa");
   p->insert(2, "bbb");
 }
 
 void test2() {
-  HeapMap<LinkedNode, std::string> *p = MakeGarbageCollected < HeapMap < LinkedNode, std::string>>();
+  HeapMap<LinkedNode, std::string> *p =
+      MakeGarbageCollected<HeapMap<LinkedNode, std::string>>();
   for (int id = 0; id < 10; id++) {
     auto t = MakeGarbageCollected<LinkedNode>(nullptr, id);
     p->insert(t, "122");
   }
-
 }
 
 void test3() {
-  HeapMap<int, LinkedNode> *p = MakeGarbageCollected < HeapMap < int, LinkedNode>>();
+  HeapMap<int, LinkedNode> *p =
+      MakeGarbageCollected<HeapMap<int, LinkedNode>>();
   for (int id = 0; id < 10; id++) {
     auto t = MakeGarbageCollected<LinkedNode>(nullptr, id);
     p->insert(id, t);
-    //p->erase(id);
+    // p->erase(id);
     std::cout << p->at(id) << std::endl;
   }
 }
 
 void test4() {
-  HeapMap<LinkedNode, LinkedNode> *p = MakeGarbageCollected < HeapMap < LinkedNode, LinkedNode>>();
+  HeapMap<LinkedNode, LinkedNode> *p =
+      MakeGarbageCollected<HeapMap<LinkedNode, LinkedNode>>();
   for (int id = 0; id < 10; id++) {
     auto t1 = MakeGarbageCollected<LinkedNode>(nullptr, id);
     auto t2 = MakeGarbageCollected<LinkedNode>(nullptr, id + 100);
 
     p->insert(t1, t2);
-    //p->erase(t1);
+    // p->erase(t1);
     std::cout << p->at(t1) << std::endl;
   }
 }
--- test/linknode.cpp
+++ test/linknode.cpp
@@ -2,18 +2,18 @@
 // Created by v4kst1z.
 //
 
-#include "../Tgc.h"
-#include <queue>
 #include <algorithm>
-#include <unordered_set>
+#include <queue>
 #include <unordered_map>
+#include <unordered_set>
+
+#include "../Tgc.h"
 
 class LinkedNode final : public GarbageCollected<LinkedNode> {
  public:
   LinkedNode(LinkedNode *next, int value) : next(next), value_(value) {}
-  void Trace(Visitor *visitor) const override {
-    visitor->Trace(next);
-  }
+  void Trace(Visitor *visitor) const override { visitor->Trace(next); }
+
  private:
   Member<LinkedNode> next;
   int value_;
@@ -34,7 +34,7 @@ int main() {
   std::unordered_map<int, int> ttt;
   ttt.insert({0, 0});
   auto ret = test();
-  //GarbageCollectedBase* x = ret->operator[](200);
+  // GarbageCollectedBase* x = ret->operator[](200);
   ret->clear();
   HeapVector<LinkedNode> *vec = MakeGarbageCollected<HeapVector<LinkedNode>>();
   for (int id = 0; id < 10; id++) {
@@ -49,15 +49,13 @@ int main() {
     LinkedNode *p = MakeGarbageCollected<LinkedNode>(nullptr, 1);
     vec->push_back(p);
     int sum = 123;
-    for (int id = 0; id < 1000; id++)
-      sum += id;
+    for (int id = 0; id < 1000; id++) sum += id;
   });
   t.join();
   WorkThread t1;
   auto res1 = t1.Run([]() {
     int sum = 0;
-    for (int id = 0; id < 1000; id++)
-      sum += id;
+    for (int id = 0; id < 1000; id++) sum += id;
     return sum;
   });
   t1.join();
